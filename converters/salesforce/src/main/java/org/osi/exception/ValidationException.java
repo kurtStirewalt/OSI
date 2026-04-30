b39421dc9ea49aa3db15a66c1316e20603634451
Nechama Deutsch <lerner.nechama@gmail.com>
2026-04-30 20:27:14 +0300

    Add OSI-Salesforce converter module

---
+++ converters/salesforce/src/main/java/org/osi/exception/ValidationException.java
@@ -0,0 +1,16 @@
+package org.osi.exception;
+
+/**
+ * Exception thrown when schema validation fails.
+ *
+ */
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
